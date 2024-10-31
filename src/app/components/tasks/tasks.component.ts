1cdb5b7d4800c38bbd5444b47d3e0b017c4f2e5d
Camilla Avila <avilacamilla@outlook.com>
2024-10-31 15:49:18 -0300

    feat: enhance TasksComponent to display task list with TaskItemComponent
    - Import TaskService and fetch tasks in ngOnInit to populate the task list
    - Add CommonModule and TaskItemComponent to imports array for rendering task items
    - Define tasks property with Task[] type to hold fetched tasks
    - Use TaskService to retrieve tasks from API and assign them to tasks array
    - Add console log for debugging fetched task data
    
    These changes allow TasksComponent to display a list of tasks, utilizing TaskItemComponent to render individual task details.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TasksService } from '../../services/tasks.service';
+import { Task } from '../../../types/task';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from "../task-item/task-item.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TaskItemComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = [];
   
-}
+  constructor(private taskService: TasksService){}
+
+  ngOnInit(): void {
+
+    this.taskService.getTasks().subscribe((data) => {
+      this.tasks =  data;
+
+      console.log(data, 'Lista de tarefas:: data')
+    })
+
+  };
+
+};
